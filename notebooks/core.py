1e0f58113605f00b67f0f849e9d36f4a8fded6d0
kshshton <michkrzyszton@gmail.com>
2025-10-05 15:31:54 +0200

    Update notebooks

--- notebooks/core.py
+++ notebooks/core.py
@@ -1,19 +1,18 @@
 import os
-import requests
 
-import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-
+import matplotlib.pyplot as plt
+import requests
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
-endpoint = os.getenv("FORECAST_ENDPOINT")
-response = requests.get(endpoint)
-json = response.json()
 
-WEEK_METADATA = json['forecast']['forecastday']
+def get_week_metadata():
+    endpoint = os.getenv("FORECAST_ENDPOINT")
+    response = requests.get(endpoint)
+    json = response.json()
+    return json['forecast']['forecastday']
 
 
 def plot(df, columns, labels, ylabel, title):
@@ -25,15 +24,26 @@ def plot(df, columns, labels, ylabel, title):
         y = df[col]
         plt.plot(x, y, label=label)
 
-    if len(lines) > 1:        
+    if len(lines) > 1:
         for idx, value in enumerate(x[df['above_threshold']]):
-            line_arguments = {'x': value, 'color': 'red', 'linestyle': '--', 'alpha': 0.7, 'zorder': 0}
+            line_arguments = {
+                'x': value,
+                'color': 'red',
+                'linestyle': '--',
+                'alpha': 0.7,
+                'zorder': 0
+            }
             if idx == 0:
                 plt.axvline(**line_arguments, label='Alert')
             plt.axvline(**line_arguments)
-    else:        
-        plt.scatter(x[df['above_threshold']], y[df['above_threshold']], color='red', zorder=5, label='Alert')
-
+    else:
+        plt.scatter(
+            x[df['above_threshold']],
+            y[df['above_threshold']],
+            color='red',
+            zorder=5,
+            label='Alert'
+        )
 
     plt.title(title)
     plt.xlabel("Time (hour)")
